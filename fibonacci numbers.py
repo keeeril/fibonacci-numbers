46d11177dd7dcf5b654dd22afb8ef076f5e5241c
Kirill <keeeril@bk.ru>
2022-02-26 00:02:31 +0300

    modified

--- fibonacci numbers.py
+++ fibonacci numbers.py
@@ -1,16 +1,3 @@
-def fib(n, counter):
-    if n ==0:
-        return 0
-    elif n ==1:
-        return 1
-    else:
-        return fib(n-1)+fib(n-2)
-
-
-
-# print(fib(4))
-
-
 def fiba(n):
     a = 0
     b = 1
